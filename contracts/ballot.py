2a20d5c7426105608fa912bedd565fc1c4eb5fbe
Noah Brooks <noahebrooks@protonmail.ch>
2022-10-03 09:10:39 -0400

    voting logic

--- contracts/ballot.py
+++ contracts/ballot.py
@@ -10,5 +10,57 @@ def approval_program():
         [Txn.application_args[0] == Bytes("vote"), on_vote],
     )
 
+    on_creation = Seq(
+        [
+            App.globalPut(Bytes("Creator"), Txn.sender()),
+            Assert(Txn.application_args.length() == Int(4)),
+            App.globalPut(Bytes("RegBegin"), Btoi(Txn.application_args[0])),
+            App.globalPut(Bytes("RegEnd"), Btoi(Txn.application_args[1])),
+            App.globalPut(Bytes("VoteBegin"), Btoi(Txn.application_args[2])),
+            App.globalPut(Bytes("VoteEnd"), Btoi(Txn.application_args[3])),
+            Return(Int(1)),
+        ],
+    )
+
+    on_register = Return(
+        And(
+            Global.round() >= App.globalGet("RegBegin"),
+            Global.round() <= App.globalGet("RegEnd"),
+        ),
+    )
+
+    get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
+
+    on_closeout = Seq(
+        [
+            get_vote_of_sender,
+            If(
+                And(
+                    Global.round() <= App.globalGet(Bytes("VoteEnd")),
+                    get_vote_of_sender.hasValue(),
+                ),
+                App.globalPut(
+                    get_vote_of_sender.value(),
+                    App.globalGet(get_vote_of_sender.value()) - Int(1),
+                ),
+            ),
+            Return(Int(1)),
+        ],
+    )
+
+    choice = Txn.application_args[1]
+    choice_tally = App.globalGet(choice)
+
+    on_vote = Seq(
+        [
+            Assert(
+                And(
+                    Global.round() >= App.globalGet(Bytes("VoteBegin")),
+
+                ),
+            ),
+        ],
+    )
+
 def clear_state_program():
     pass
